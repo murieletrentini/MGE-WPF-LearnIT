a26a02b84e1f5f979c498911dcc46bc05d89c6b5
Nico Vinzens <Nico Vinzens>
2016-12-01 15:27:02 +0100

    added db.cs

---
+++ MGE-WPF-LearnIT/domain/Db.cs
@@ -0,0 +1,14 @@
+﻿using MGE_WPF_LearnIT.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGE_WPF_LearnIT.domain {
+    class Db : DbContext {
+        public DbSet<CardSet> CardSets { get; set; }
+        public DbSet<Card> Cards { get; set; }
+    }
+}
